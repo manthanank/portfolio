b31f8742a13cc3fac0083afd7131c787ee47954a
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-11 00:39:22 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,10 @@ import { ErrorComponent } from './pages/error/error.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ProjectDetailsComponent } from './pages/projects/project-details/project-details.component';
 import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -40,6 +44,10 @@ import { environment } from '../environments/environment';
     HttpClientModule,
     NgOptimizedImage,
     UiSwitchModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'manthanank',
+    appId: '1:155323854842:web:8dd40edf91f9839ca7be43',
+    storageBucket: 'manthanank.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDlWzvBtERyiJqL4hlFsCmXG83j3WRLAcM',
+    authDomain: 'manthanank.firebaseapp.com',
+    messagingSenderId: '155323854842',
+  },
   production: false,
 };
