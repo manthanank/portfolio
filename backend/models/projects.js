158fdf237d9e1ed8e8ecaeac59bae3fb5095ba6c
Manthan Ankolekar <manthan.ank@gmail.com>
2023-04-02 16:00:28 +0530

    feat: addes desc in projects

--- backend/models/projects.js
+++ backend/models/projects.js
@@ -4,7 +4,8 @@ const Projects = mongoose.model('projects', {
     title : {type: String},
     codeurl: {type: String},
     previewurl: {type: String},
-    imgurl: {type: String}
+    imgurl: {type: String},
+    desc: {type: String}
 });
 
 module.exports = Projects;
