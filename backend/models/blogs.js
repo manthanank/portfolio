7b2faf5116b045ddc3aad7c8256ed24d10ea7091
Manthan Ankolekar <manthan.ank@gmail.com>
2023-04-09 13:07:07 +0530

    fix: deleted backend

--- backend/models/blogs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Blogs = mongoose.model('blogs', {
-    title : {type: String},
-    url: {type: String},
-    imgurl: {type: String}
-});
-
-module.exports = Blogs;
--- backend/models/uses.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const Uses = mongoose.model('uses', {
-    laptop : {type: String}
-});
-
-module.exports = Uses;
